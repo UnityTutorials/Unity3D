a3b96e6705f1490b7991e62d464815fd30b20c14
vagedeja <vagedeja@mail.uc.edu>
2016-12-28 11:59:26 -0500

    v0.001
    +How To Detect Objects

---
+++ Assets/HowToDetectObjects/ViewObjects.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ViewObjects : MonoBehaviour {
+
+    public int RAD;
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    private void DetectObjects()
+    {
+        Collider[] myDetectedObjects = Physics.OverlapSphere(transform.position, RAD);
+
+        int i = 0;
+        while (i < myDetectedObjects.Length && myDetectedObjects[i].gameObject.transform.tag != "Player")
+        {
+            // Do whatever you want with these detected colliders.
+
+            Debug.Log("I See You!");
+
+            i++;
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        DetectObjects();
+    }
+}
